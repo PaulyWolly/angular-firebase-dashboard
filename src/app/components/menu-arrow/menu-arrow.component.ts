69d757d4a8978289681c7117a1f04927b7633425
Paul Welby <pwelby@gmail.com>
2022-03-19 21:06:08 -0700

    converted the dropdown menu to a component

--- src/app/components/menu-arrow/menu-arrow.component.ts
+++ src/app/components/menu-arrow/menu-arrow.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-menu-arrow',
   templateUrl: './menu-arrow.component.html',
   styleUrls: ['./menu-arrow.component.css']
 })
-export class MenuArrowComponent {
+export class MenuArrowComponent implements OnInit {
 
-  showMore: boolean = false;
+  user: Observable<any>;
+  showMore: Boolean = false;
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    private firestore: AngularFirestore,
+    public route: Router
+    ) { }
+
+
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => {
+      console.log('Dashboard: user', user);
+
+      if (user) {
+          let emailLower = user.email.toLowerCase();
+          this.user = this.firestore.collection('users').doc(emailLower).valueChanges();
+      }
+    });
+  }
 
   showResult(){
     this.showMore = true;
